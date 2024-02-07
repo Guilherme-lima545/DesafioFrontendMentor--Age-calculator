8c3ccaccce81191e43b2f55b3c026f2e3dbbd27e
Guilherme Alves de Lima <114428628+Guilherme-lima545@users.noreply.github.com>
2024-02-07 20:18:32 -0300

    Add files via upload

---
+++ js/calendar.js
@@ -0,0 +1,102 @@
+const output_year = document.querySelector(".output-year");
+const output_month = document.querySelector(".output-month");
+const output_day = document.querySelector(".output-day");
+const seta = document.querySelector (".seta");
+
+let isValid = false
+const input_year = document.querySelector("#year");
+const input_month = document.querySelector("#month");
+const input_day = document.querySelector("#day");
+
+const error_year = document.querySelector(".error-year");
+const error_month = document.querySelector(".error-month");
+const error_day = document.querySelector("error-day");
+
+seta.addEventListener("click", CalculateDate)
+
+input_day.addEventListener("input", (e) => {
+    if (+input_day.value > 31) {
+        error_day.textContent = "Essa não é uma data valida";
+        isValid = false;
+        return;
+    }
+    else {
+        isValid = true;
+        error_day.textContent = "";
+    }
+    if(+input_day.value === 0) {
+        isValid = false;
+        error_day.textContent = "Insira uma data valida";
+        isValid = false;
+        return;
+    }
+    else {
+        error_day.textContent = "";
+    }
+
+});
+
+input_month.addEventListener("input", (e) => {
+    if (+input_month.value > 12) {
+        error_month.textContent = "Essa não é uma data valida";
+        isValid = false;
+        return;
+    }
+    else {
+        isValid = true;
+        error_month.textContent = "";
+    }
+    if(+input_month.value === 0) {
+        isValid = false;
+        error_month.textContent = "Insira uma data valida";
+        isValid = false;
+        return;
+    }
+    else {
+        error_month.textContent = "";
+    }
+});
+
+
+input_year.addEventListener("input", (e) => {
+    if (+input_year.value > 2024) {
+        error_year.textContent = "Essa não é uma data valida";
+        isValid = false;
+        return;
+    }
+    else {
+        isValid = true;
+        error_year.textContent = "";
+    }
+    if(+input_year.value === 0) {
+        isValid = false;
+        error_year.textContent = "Insira uma data valida";
+        isValid = false;
+        return;
+    }
+    else {
+        error_year.textContent = "";
+    }
+});
+
+
+function CalculateDate () {
+    if(isValid) {
+        let birthday = `${input_month.value}/${input_day.value}/${input_year.value}`;
+        console.log(birthday)
+        let birthdayObj = new Date(birthday);
+        let ageDiffMill = Date.now() - birthdayObj;
+        let ageDate = new Date(ageDiffMill);
+        let ageYears = ageDate.getUTCFullYear() - 1970;
+        let ageMonth = ageDate.getUTCMonth();
+        let ageDay = ageDate.getUTCDay();
+        
+        output_day.textContent = ageDay;
+        output_month.textContent = ageMonth;
+        output_year.textContent = ageYears;
+    }
+    else {
+        alert("error");
+    }
+}
+
